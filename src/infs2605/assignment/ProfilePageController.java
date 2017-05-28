59db5d0e45bcd3ad1f5978948a47f776ee5b0375
yeahokpaul <hamishccowan@westnet.com.au>
2017-05-28 15:16:49 +1000

    3:16pm

--- src/infs2605/assignment/ProfilePageController.java
+++ src/infs2605/assignment/ProfilePageController.java
@@ -7,6 +7,7 @@ package infs2605.assignment;
 
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,8 +16,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -27,7 +32,6 @@ import javafx.stage.Stage;
  * @author paulstrasser
  */
 public class ProfilePageController implements Initializable {
-
     @FXML
     Stage stage;
     Parent root;
@@ -66,6 +70,105 @@ public class ProfilePageController implements Initializable {
     private TitledPane Payment;
     
     @FXML
+    private Text fName;
+    
+    @FXML
+    private TextField lName;
+    
+    @FXML
+    private DatePicker dob;
+    
+    @FXML
+    private TextField hPhone;
+    
+    @FXML
+    private TextField wPhone;
+    
+    @FXML
+    private TextField mPhone;
+    
+    @FXML
+    private TextField email;
+    
+    @FXML
+    private TextField hNum;
+    
+    @FXML
+    private TextField hStreet;
+    
+    @FXML
+    private TextField hSuburb;
+    
+    @FXML
+    private TextField hCity;
+    
+    @FXML
+    private TextField hPostCode;
+    
+    @FXML
+    private TextField wNum;
+    
+    @FXML
+    private TextField wStreet;
+    
+    @FXML
+    private TextField wSuburb;
+    
+    @FXML
+    private TextField wCity;
+    
+    @FXML
+    private TextField wPostCode;
+    
+    @FXML
+    private TextField make;
+    
+    @FXML
+    private TextField model;
+    
+    @FXML
+    private ComboBox colour;
+        
+    @FXML
+    private TextField yearMade;
+    
+    @FXML
+    private TextField registration;
+    
+    @FXML
+    private ComboBox numOfSeats;
+    
+    @FXML
+    private TextField licenseNum;
+    
+    @FXML
+    private DatePicker expDate;
+    
+    @FXML
+    private TextField userName;
+    
+    @FXML
+    private PasswordField passWord;
+    
+    @FXML
+    private TextField description;
+    
+    @FXML
+    private TextField nameOnCard;
+    
+    @FXML
+    private TextField cardNum;
+    
+    @FXML
+    private DatePicker cardExpiry;
+    
+    @FXML
+    private TextField cVV;
+    
+    @FXML
+    private Text userType;    
+    
+    @FXML
     private Text Name;
     
     DBController d = new DBController();
@@ -79,7 +182,44 @@ public class ProfilePageController implements Initializable {
         LicenseDetails.setCollapsible(false);
         Payment.setCollapsible(false); 
         Name.setText(d.returnSingleQuery("SELECT FNAME AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
-
+        fName.setText(d.returnSingleQuery("SELECT FNAME AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        description.setText(d.returnSingleQuery("SELECT DESCRIPTION AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        lName.setText(d.returnSingleQuery("SELECT LNAME AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        userType.setText(d.returnSingleQuery("SELECT USERTYPE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        LocalDate DOB = LocalDate.parse(d.returnSingleQuery("SELECT DOB AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        dob.setValue(DOB);
+        hPhone.setText(d.returnSingleQuery("SELECT HPHONE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        wPhone.setText(d.returnSingleQuery("SELECT WPHONE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        mPhone.setText(d.returnSingleQuery("SELECT MPHONE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        email.setText(d.returnSingleQuery("SELECT EMAIL AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        hNum.setText(d.returnSingleQuery("SELECT HNUM AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        hStreet.setText(d.returnSingleQuery("SELECT HSTREET AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        hSuburb.setText(d.returnSingleQuery("SELECT HSUBURB AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        hCity.setText(d.returnSingleQuery("SELECT HCITY AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        hPostCode.setText(d.returnSingleQuery("SELECT HPOSTCODE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        wNum.setText(d.returnSingleQuery("SELECT WNUM AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        wStreet.setText(d.returnSingleQuery("SELECT WSTREET AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        wSuburb.setText(d.returnSingleQuery("SELECT WSUBURB AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        wCity.setText(d.returnSingleQuery("SELECT WCITY AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        wPostCode.setText(d.returnSingleQuery("SELECT WPOSTCODE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        make.setText(d.returnSingleQuery("SELECT MAKE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        model.setText(d.returnSingleQuery("SELECT MODEL AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        colour.setValue(d.returnSingleQuery("SELECT COLOUR AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        yearMade.setText(d.returnSingleQuery("SELECT YEARMADE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        registration.setText(d.returnSingleQuery("SELECT REGISTRATION AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        numOfSeats.setValue(d.returnSingleQuery("SELECT NUMOFSEATS AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        licenseNum.setText(d.returnSingleQuery("SELECT LICENSENUM AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        LocalDate ExpiryDate = LocalDate.parse(d.returnSingleQuery("SELECT EXPIRYDATE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        expDate.setValue(ExpiryDate);
+        userName.setText(d.returnSingleQuery("SELECT USERNAME AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        passWord.setText(d.returnSingleQuery("SELECT PASSWORD AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        nameOnCard.setText(d.returnSingleQuery("SELECT NAMEONCARD AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        cardNum.setText(d.returnSingleQuery("SELECT CARDNUM AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        LocalDate CardExpiry = LocalDate.parse(d.returnSingleQuery("SELECT EXPIRYDATE AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        cardExpiry.setValue(CardExpiry);
+        cVV.setText(d.returnSingleQuery("SELECT CVV AS ANSWER FROM USER WHERE USERNAME LIKE '" + SignInController.getUser() + "'"));
+        colour.getItems().addAll("Blue", "Red", "Green", "Black", "Silver", "White", "Orange");
+        numOfSeats.getItems().addAll("1", "2", "3", "4", "5", "6");
     }   
     
     @FXML
